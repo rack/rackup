be545952432d0f03c954c56413a9535212ac71d5
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-11-02 15:50:40 +1300

    Soft deprecate webrick and add some details as to why. (#23)

--- lib/rackup.rb
+++ lib/rackup.rb
@@ -6,6 +6,3 @@
 require_relative 'rackup/handler'
 require_relative 'rackup/server'
 require_relative 'rackup/version'
-
-require_relative 'rackup/handler/webrick'
-require_relative 'rackup/handler/cgi'
