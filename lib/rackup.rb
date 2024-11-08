db13822cadfe4ea76795d232064e3fe5fd289ff1
Earlopain <14981592+Earlopain@users.noreply.github.com>
2024-11-08 20:21:02 +0100

    Require the webrick handler when webrick is available
    Followup to #23
    
    Even when users do add webrick to their gemfile, the handler is not available.
    This means that they either need to do that themselves, or dependencies need to
    be updated.

--- lib/rackup.rb
+++ lib/rackup.rb
@@ -6,3 +6,16 @@
 require_relative 'rackup/handler'
 require_relative 'rackup/server'
 require_relative 'rackup/version'
+
+begin
+  # Although webrick is gone from Ruby since 3.0, it still warns all the way
+  # through to 3.3. Only on 3.4 will requiring it not produce a warning anymore.
+  verbose, $VERBOSE = $VERBOSE, nil
+  require 'webrick'
+  # If the user happens to have webrick in their bundle, make the handler available.
+  require_relative 'rackup/handler/webrick'
+rescue LoadError
+  # ¯\_(ツ)_/¯
+ensure
+  $VERBOSE = verbose
+end
